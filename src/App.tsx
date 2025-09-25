224e91d4e4e076fb1b7070b74ddf99ecfe8aa4d6
Mehdi Rahimi <mehdii.rh78@gmail.com>
2025-09-25 20:37:53 +0330

    [UPDATE] add more sections

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,88 @@
-import '@/App.css'
 import PixelBlast from "@/backgrounds/third-party/PixelBlast/PixelBlast.tsx";
-import SplitText from "@/components/third-party/SplitText/SplitText.tsx";
-import Button from "@/components/Button/Button.tsx";
+import {useRef} from "react";
+import RotatingText from "@/components/third-party/RotatingText/RotatingText.tsx";
+import {Box, Center, Text, useBreakpointValue} from "@chakra-ui/react";
+import Background from "@/components/Section/Background/Background.tsx";
+import Foreground, {ForegroundContent} from "@/components/Section/Foreground/Foreground.tsx";
+import Button from './components/Button/Button';
+import MagicBento from "@/backgrounds/third-party/MagicBento/MagicBento.tsx";
 
 function App() {
+  
+  const sectionTwo = useRef<HTMLDivElement | null>(null);
+  
   return (
-    <div>
-      <div className="background">
+    <Box>
+      {/*SECTION ONE START*/}
+      <Box as={Background} className="h-[90dvh] section-one">
         <PixelBlast/>
-        <div className="foreground center font-bold">
-          <div className="foreground-content">
-            <SplitText tag="h1" text="Show You Everywhere !"/>
-            <div className="flex flex-col md:flex-row gap-3 justify-center">
-              <Button className="fill">Default</Button>
-              <Button className="outline">Disabled</Button>
-            </div>
-          
-          </div>
-        </div>
-      </div>
-    </div>
+        <Foreground>
+          <ForegroundContent className="gap-10">
+            <Center className="flex-col md:flex-row" gap={4}>
+              <Text
+                overflow="hidden"
+                style={{fontSize: useBreakpointValue({base: "15dvw", md: "4dvw"})}}
+              >
+                Showup in
+              </Text>
+              <Box fontSize={{base: "17dvw", md: "4dvw"}}>
+                <RotatingText
+                  texts={['Channels', 'Bots', 'Search']}
+                  mainClassName="px-2 sm:px-2 md:px-3 text-white w-fit bg-[#5227ff] overflow-hidden py-0.5 justify-center rounded-2xl"
+                  staggerFrom={"last"}
+                  initial={{y: "100%"}}
+                  animate={{y: 0}}
+                  exit={{y: "-120%"}}
+                  staggerDuration={0.025}
+                  splitLevelClassName="overflow-hidden pb-0.5 sm:pb-1 md:pb-1"
+                  transition={{type: "spring", damping: 30, stiffness: 400}}
+                  rotationInterval={2000}
+                />
+              </Box>
+            </Center>
+            <Text fontWeight="light" letterSpacing={1}>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Quibusdam, vero.</Text>
+            <Button
+              onClick={() => window.open("https://panel.wise.ad/", "_blank")}
+              className="btn fill w-1/2 md:w-1/4 lg:w-1/6">
+              Login
+            </Button>
+          </ForegroundContent>
+        </Foreground>
+      </Box>
+      {/*SECTION ONE END*/}
+      
+      {/*SECTION TWO START*/}
+      <Box as={Background} ref={sectionTwo} className="h-full section-one">
+        <Center className="gap-10">
+          <MagicBento
+            textAutoHide={true}
+            enableStars={true}
+            enableSpotlight={true}
+            enableBorderGlow={true}
+            enableTilt={true}
+            enableMagnetism={true}
+            clickEffect={true}
+            spotlightRadius={300}
+            particleCount={12}
+            glowColor="132, 0, 255"
+          />
+        </Center>
+      </Box>
+      {/*SECTION TWO END*/}
+      
+      {/*THIRD TWO START*/}
+      {/*THIRD TWO END*/}
+      
+      {/*FOURTH TWO START*/}
+      {/*FOURTH TWO START*/}
+      
+      {/*FIFTH TWO END*/}
+      {/*FIFTH TWO END*/}
+      
+      {/*FOOTER START*/}
+      {/*FOOTER END*/}
+    </Box>
   )
 }
 
